dfac2c220a3458298e02544b441e2cd1ed17f681
Johan Guse <johanguse@gmail.com>
2024-06-11 17:27:08 -0300

    feat: update price card

--- src/components/pricing/pricing-one-card.tsx
+++ src/components/pricing/pricing-one-card.tsx
@@ -7,9 +7,9 @@ export default function OneCardPricingTable() {
             One time purchase
           </h1>
           <p className="mt-6 text-base leading-7 text-gray-600 dark:text-gray-400">
-            Choose our monthly subscription plan that would suit your extra
-            needs. Cancel anytime. Custom enterprise pricing is available upon
-            request.{' '}
+            Buy now and receive the full code immediately.
+            <br />
+            Have question?
             <a
               href="/contact"
               className="text-blue-500 underline underline-offset-4 dark:text-blue-400"
@@ -28,9 +28,12 @@ export default function OneCardPricingTable() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
+                zoomAndPan="magnify"
                 viewBox="0 0 30 30.000001"
                 height="20"
-                className="mr-1"
+                preserveAspectRatio="xMidYMid meet"
+                version="1.0"
+                id="IconChangeColor"
               >
                 <defs>
                   <clipPath id="id1">
@@ -52,8 +55,8 @@ export default function OneCardPricingTable() {
                   ></path>
                 </g>
               </svg>{' '}
-              <span className="font-bold">1000</span>
-              <span>Resumes / Check</span>
+              <span className="mx-1 font-bold">Support</span>
+              <span>by Email</span>
             </li>
             <li className="flex items-center">
               <svg
@@ -86,8 +89,8 @@ export default function OneCardPricingTable() {
                   ></path>
                 </g>
               </svg>{' '}
-              <span className="mx-1 font-bold">30</span>
-              <span>Job Posts</span>
+              <span className="mx-1 font-bold">Github</span>
+              <span>Access</span>
             </li>
             <li className="flex items-center">
               <svg
@@ -120,8 +123,8 @@ export default function OneCardPricingTable() {
                   ></path>
                 </g>
               </svg>{' '}
-              <span className="mx-1 font-bold">24/7</span>
-              <span>Priority Support</span>
+              <span className="mx-1 font-bold">Lifetime</span>
+              <span>Updates</span>
             </li>
             <li className="flex items-center">
               <svg
@@ -154,42 +157,8 @@ export default function OneCardPricingTable() {
                   ></path>
                 </g>
               </svg>{' '}
-              <span className="mx-1 font-bold"></span>
-              <span>Custom Hosting</span>
-            </li>
-            <li className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                zoomAndPan="magnify"
-                viewBox="0 0 30 30.000001"
-                height="20"
-                preserveAspectRatio="xMidYMid meet"
-                version="1.0"
-                id="IconChangeColor"
-              >
-                <defs>
-                  <clipPath id="id1">
-                    <path
-                      d="M 2.847656 4.792969 L 26.796875 4.792969 L 26.796875 24.390625 L 2.847656 24.390625 Z M 2.847656 4.792969 "
-                      clipRule="nonzero"
-                      id="mainIconPathAttribute"
-                      fill="green"
-                    ></path>
-                  </clipPath>
-                </defs>
-                <g clipPath="url(#id1)">
-                  <path
-                    fill="green"
-                    d="M 11.078125 24.3125 L 2.847656 15.890625 L 6.128906 12.53125 L 11.078125 17.597656 L 23.519531 4.875 L 26.796875 8.230469 Z M 11.078125 24.3125 "
-                    fillOpacity="1"
-                    fillRule="nonzero"
-                    id="mainIconPathAttribute"
-                  ></path>
-                </g>
-              </svg>{' '}
-              <span className="mx-1 font-bold"></span>
-              <span>White Label Access</span>
+              <span className="mx-1 font-bold">Pay once.</span>
+              <span>Build unlimited projects</span>
             </li>
           </ul>
         </div>
@@ -201,17 +170,18 @@ export default function OneCardPricingTable() {
               </p>
               <p className="mb-12 mt-6 flex items-baseline justify-center gap-x-2">
                 <span className="text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <span className="px-2 line-through">$199</span>
-                  <span className="italic text-gray-700 dark:text-gray-500">
-                    $129.35
+                  <span className="px-2 text-gray-700 line-through dark:text-gray-500">
+                    $199
                   </span>
+                  <span className="italic">$129.35</span>
                 </span>
                 <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600 dark:text-gray-400">
                   USD
                 </span>
               </p>
               <a
-                href="https://buy.stripe.com/bIY9BE2cvgFb8CI288?prefilled_promo_code=LAUNCH"
+                //href="https://buy.stripe.com/bIY9BE2cvgFb8CI288?prefilled_promo_code=LAUNCH"
+                href="https://bit.ly/NST-LAUNCH1"
                 className="my-6 block w-full rounded-sm bg-blue-600 py-2 text-white dark:bg-white dark:text-gray-800"
               >
                 Get Started →
